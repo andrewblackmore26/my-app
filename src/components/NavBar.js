6d1ce6bfb7f0271f38fd658c1375c7a665e8bafa
MrBlackmo <andrewblackmore95@gmail.com>
2022-11-06 00:23:32 +1100

    personal website start - early draft

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import '../styles/NavBar.css'
+
+function NavBar() {
+
+  return (
+    <div>
+      <h1>AB - Personal Portfolio</h1>
+      <a href="#">Projects</a>
+      <a href="#">Skills</a>
+    </div>
+  )
+}
+
+export default NavBar
