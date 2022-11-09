658488b5fc7e8838fb8afb28118942b7f29f059f
MrBlackmo <andrewblackmore95@gmail.com>
2022-11-10 01:00:07 +1100

    we back - fixed webapp crash caused by using component name in return

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
 import React from "react"
 import '../styles/NavBar.css'
+import { Navbar } from "react-bootstrap"
 
 function NavBar() {
 
   return (
-    <div>
+    <Navbar>
       <h1>AB - Personal Portfolio</h1>
       <a href="#">Projects</a>
       <a href="#">Skills</a>
-    </div>
+    </Navbar>
   )
 }
 
