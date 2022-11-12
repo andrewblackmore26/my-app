7cede7df8f7ee1cac7537cc91fdccc941c79535f
MrBlackmo <andrewblackmore95@gmail.com>
2022-11-12 20:46:58 +1100

    my-app

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from "react"
 import '../styles/NavBar.css'
-import { Navbar } from "react-bootstrap"
+import { Navbar, Nav, Container } from "react-bootstrap"
 
 function NavBar() {
 
   return (
     <Navbar>
       <h1>AB - Personal Portfolio</h1>
-      <a href="#">Projects</a>
-      <a href="#">Skills</a>
+      <Nav.Link className="navbar-link" href="#">Projects</Nav.Link>
+      <Nav.Link className="navbar-link" href="#">Skills</Nav.Link>
     </Navbar>
   )
 }
