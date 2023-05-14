13fba239947bb396fdad33347efb7d8b327cac5a
MrBlackmo <andrewblackmore95@gmail.com>
2023-05-14 23:13:52 +1000

    added icons (links to socials)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../styles/Home.css';
+import {FaInstagram, FaGithub} from "react-icons/fa";
+import {FiMail} from "react-icons/fi";
 
 function Home() {
   return (
@@ -8,11 +10,21 @@ function Home() {
       <p>Here you can find information about me and my work <br></br>
       Whether it be new projects or new experiences, these events will be uploaded/linked to here <br></br>
       Currently I am a student at UNSW, graduating in the year 2024 <br></br>
-      I have a wide range of experiences, ranging from entrepreneur, content creator and traveller<br></br>
-      And with the huge changes coming from tech in the near future, I plan to ride that wave<br></br>
+      I have a wide range of experiences, ranging from entrepreneur, content creator and programmer<br></br>
+      With the huge changes coming from tech in the near future, I plan to ride that wave<br></br>
       And share those experiences with you guys!<br></br>
-      </p>
-
+      </p>     
+      <div className="icons">
+      <a  href="https://www.instagram.com/andrewblackmore0" target="_blank">
+      <FaInstagram className="icon" />
+      </a>
+      <a href="https://github.com/andrewblackmore26" target="_blank">
+      <FaGithub className="icon" />
+      </a>
+      <a href="mailto:andrewblackmore95@gmail.com" target="_blank">
+      <FiMail className="icon" />
+      </a>
+      </div>
     </div>
   );
 }
