446eb21b8a382a837e6c400597a300badcc221dc
MrBlackmo <andrewblackmore95@gmail.com>
2023-05-17 20:30:21 +1000

    added carousel to skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,19 +1,63 @@
 import React from "react"
 import '../styles/Skills.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Col, Row, Container } from "react-bootstrap";
+
 
 function Skills() {
-
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
 
   return (
-    <div className="skills-container" name="skills">
-      <h2>Welcome to my portfolio website</h2>
-      <p>Here you can find information about me and my work <br></br>
-      Whether it be new projects or new experiences, these events will be uploaded/linked to here <br></br>
-      Currently I am a student at UNSW, graduating in the year 2024 <br></br>
-      I have a wide range of experiences, ranging from entrepreneur, content creator and programmer<br></br>
-      With the huge changes coming from tech in the near future, I plan to ride that wave<br></br>
-      And share those experiences with you guys!<br></br>
-      </p>     
+    <div className="skills-container" name="skills">  
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>
+                Skills
+              </h2>
+              <p>Lorem ipsum is simply summy text of the printing and typesetting industry</p>
+              <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                <div className="item">
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+
+                  <h5>React</h5>
+                </div>
+                <div className="item">
+
+                  <h5>Python</h5>
+                </div>
+                <div className="item">
+
+                  <h5>Java</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <footer className="footer">
+        <p>Footer content goes here</p>
+      </footer>
     </div>
   )
 }
